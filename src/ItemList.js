cf99508c7f5e3bf3e4c1e37a9bb49cfa72ff1e3e
ememer <46853050+ememer@users.noreply.github.com>
2021-04-09 14:29:17 +0200

    x

--- src/ItemList.js
+++ src/ItemList.js
@@ -14,7 +14,7 @@ const ItemList = (props) => {
   });
 
   useEffect(() => {
-    const promis = new Promise((resolve, reject) => {
+    const personPromise = new Promise((resolve, reject) => {
       if (props.person) {
         resolve(props.person);
       } else {
@@ -22,7 +22,7 @@ const ItemList = (props) => {
       }
     });
 
-    promis.then((res) => {
+    personPromise.then((res) => {
       setPropsData({
         id: res.id,
         firstName: res.firstName,
@@ -32,11 +32,36 @@ const ItemList = (props) => {
         checked: res.check,
       });
     });
-  }, [props.person, checkBoxStatus]);
 
-  const handleToggleCheck = () => {
+    const checkedPromise = new Promise((resolve, reject) => {
+      if (props.isContactChecked) {
+        resolve(props.isContactChecked);
+      } else {
+        console.log(props.isContactChecked);
+        reject("Przesłane dane nie są obiektem");
+      }
+    });
+
+    checkedPromise.then((data) => {
+      const contact = data.map((contactId) => {
+        return contactId.id;
+      });
+
+      if (contact.toString() === propsData.id.toString()) {
+        // console.log("das", ...contact, propsData.id);
+        setCheckBoxStatus(true);
+      }
+    });
+  }, [props.person, checkBoxStatus, props.isContactChecked, propsData.id]);
+
+  const handleToggleCheck = (e) => {
+    console.log(e.target.id, propsData.id);
     setCheckBoxStatus((prevState) => !prevState);
-    console.log(props.person.check);
+    if (!checkBoxStatus) {
+      props.get(e.target.id, true);
+    } else {
+      props.get(e.target.id, false);
+    }
   };
 
   return (
@@ -63,6 +88,7 @@ const ItemList = (props) => {
           checked={checkBoxStatus}
           className="col-2 checkbox"
           type="checkbox"
+          id={propsData.id}
         ></input>
       </li>
       {checkBoxStatus ? (
--- src/List.js
+++ src/List.js
@@ -1,10 +1,31 @@
+import { useState } from "react";
 import ItemList from "./ItemList";
 
 const List = (props) => {
+  const [checkedContact, setCheckedContact] = useState("");
+
+  function getCheckedContact(id, check) {
+    if (check) {
+      setCheckedContact((prevState) => [
+        ...prevState,
+        { id: id, check: check },
+      ]);
+    } else if (!check) {
+      setCheckedContact((prevState) =>
+        prevState.filter((person) => person.id !== id && person.check !== check)
+      );
+    }
+  }
+
   return (
     <ul className="container">
       {props.response.map((person) => (
-        <ItemList key={person.id} person={person} />
+        <ItemList
+          isContactChecked={checkedContact}
+          get={getCheckedContact}
+          key={person.id}
+          person={person}
+        />
       ))}
     </ul>
   );
